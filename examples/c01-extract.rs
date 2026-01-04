66835e208e921a92403627f553c4f68ca5be314e
Jeremy Chone <jeremy.chone@gmail.com>
2026-01-03 19:02:50 -0800

    . depdencies - remove tokio, add tracing

--- examples/c01-extract.rs
+++ examples/c01-extract.rs
@@ -1,11 +1,11 @@
+use std::fs;
 use udiffx::extract_file_changes;
 
 type Result<T> = core::result::Result<T, Box<dyn std::error::Error>>;
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
 	let file_path = "tests/data/changes-simple.md";
-	let content = tokio::fs::read_to_string(file_path).await?;
+	let content = fs::read_to_string(file_path)?;
 
 	let changes = extract_file_changes(&content, false)?.0;
 
